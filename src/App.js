07d0d74cc6b5fded2521b93277d11e05c3631e16
Shahzad Ahmed <shahzad.btm@gmail.com>
2020-06-08 08:47:19 +0500

    Added Hello.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
 // import logo from './logo.svg';
 import './App.css';
+import './Hello.js';
+import Hello from './Hello.js';
 
-function App() {
+function App(props) {
   return (
     <div className="App">
      <h1>Hello World, again...</h1>
+  <h2>Name: {props.name}</h2>
+  <h2>Age: {props.age}</h2>
+  <Hello></Hello>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App name="Shahzad Ahmed" age="34"/>
   </React.StrictMode>,
   document.getElementById('root')
 );
